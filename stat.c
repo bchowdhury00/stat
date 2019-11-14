18f2af2da47e0bd07ee5da9e45faac554a20318f
Biraj Chowdhury <birajchowdhury@Birajs-MBP.nyc.rr.com>
2019-11-13 23:39:38 -0500

    try

--- stat.c
+++ stat.c
@@ -2,9 +2,26 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <string.h>
+#include <time.h>
 
 int main(){
   struct stat * metadata;
   stat("testFile.txt",metadata);
-  printf("%ld",metadata->st_size);
+  long size = metadata->st_size;
+  int counter = 0;
+  while ((size % 1024) > 1024){
+    size /= 1024;
+    counter++;
+  }
+  char * unit = "B";
+  if (counter == 1)
+    unit = "KB";
+  if (counter == 2)
+    unit = "MB";
+  if (counter ==3)
+    unit = "GB";
+  printf("%ld %s\n",size,unit);
+  printf("%o\n",metadata->st_mode);
+  printf("Time last accessed : %s\n",ctime(&(metadata->st_atime)));
+  
 }
